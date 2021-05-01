605b16b8c5d52d308cc44db2c8652dd07c0d9bdd
SungYinYang <sungyin.yang@sjsu.edu>
2021-05-01 17:16:28 -0500

    update commit

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,19 +1,20 @@
-import React, { useState, useEffect } from "react"
-import { Card, Button, Alert, Container, Row, Col } from "react-bootstrap"
-import { useAuth } from "../context/AuthContext"
-import { Link, useHistory } from "react-router-dom"
-import { saveStocksToDB, readStocksFromDB } from "../firebase";
+import React, { useState, useEffect } from 'react';
+import { Card, Button, Alert, Container, Row, Col } from 'react-bootstrap';
+import { useAuth } from '../context/AuthContext';
+import { Link, useHistory } from 'react-router-dom';
+import { saveStocksToDB, readStocksFromDB } from '../firebase';
 import StockSearch from './StockSearch';
 import StockList from './StockList';
 import InvestmentSankey from './InvestmentSankey';
-import Tableau from "./TableauEmbed.js"
-import "./Dashboard.css";
-
+import Tableau from './TableauEmbed.js';
+import './Dashboard.css';
+import Tabs from './Tabs';
+import './Tab.css';
 
 export default function Dashboard() {
-  const [error, setError] = useState("")
-  const { currentUser, logout } = useAuth()
-  const history = useHistory()
+  const [error, setError] = useState('');
+  const { currentUser, logout } = useAuth();
+  const history = useHistory();
   const [stocks, setStocks] = useState([]);
   // the user's total investment
   const [totalInvestment, setTotalInvestment] = useState(0);
@@ -26,9 +27,8 @@ export default function Dashboard() {
   useEffect(() => {
     var stocks = readStocksFromDB(currentUser, onDataRead);
     //console.log(s);
-    
-    //s.on("value", onDataRead);
 
+    //s.on("value", onDataRead);
   }, []); // empty array runs useEffect only once
 
   // callback function for when the data loads on backend
@@ -43,110 +43,110 @@ export default function Dashboard() {
         symbol: item.symbol,
         price: item.price,
         investment: item.investment,
-        date: item.date
+        date: item.date,
       });
       console.log(item.symbol);
       getStockOpen(item.symbol);
     });
     setStocks(stocks);
-  }
+  };
 
   async function handleLogout() {
-    setError("")
+    setError('');
 
     try {
-      await logout()
-      history.push("/login")
+      await logout();
+      history.push('/login');
     } catch {
-      setError("Failed to log out")
+      setError('Failed to log out');
     }
   }
 
-
-  async function onSymbolSubmit( input ) {
-
+  async function onSymbolSubmit(input) {
     // add to total investment
     setTotalInvestment(totalInvestment + parseInt(input.investment));
     console.log(totalInvestment);
-    // get time series daily 
+    // get time series daily
     let API_Call = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${input.symbol}&outputsize=full&apikey=${API_KEY}`;
     console.log(API_Call);
     fetch(API_Call)
-      .then(
-        function (response) {
-          return response.json();
-        }
-      )
-      .then(
-        function (data) {
-          let currentDate = getCurrentWeekday();
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        let currentDate = getCurrentWeekday();
 
-          
-          let timeSeries = data["Time Series (Daily)"];
-          let openPrice = Object.keys(timeSeries).reduce(function(a, b){ return timeSeries[a] > timeSeries[b] ? b : a });
-          console.log(openPrice);
+        let timeSeries = data['Time Series (Daily)'];
+        let openPrice = Object.keys(timeSeries).reduce(function (a, b) {
+          return timeSeries[a] > timeSeries[b] ? b : a;
+        });
+        console.log(openPrice);
 
-          // Verify that the stock symbol is the same
-          // console.log(data["Meta Data"]["2. Symbol"]);
-          // get the current date's stock open price
-          console.log(currentDate, data["Time Series (Daily)"][openPrice]);
-          let newStock = {
-            symbol: data["Meta Data"]["2. Symbol"],
-            price: data["Time Series (Daily)"][openPrice]["1. open"],
-            investment: input.investment,
-            date: currentDate
-          }
-          // TODO: use stock symbol here to use for tableau interface?
+        // Verify that the stock symbol is the same
+        // console.log(data["Meta Data"]["2. Symbol"]);
+        // get the current date's stock open price
+        console.log(currentDate, data['Time Series (Daily)'][openPrice]);
+        let newStock = {
+          symbol: data['Meta Data']['2. Symbol'],
+          price: data['Time Series (Daily)'][openPrice]['1. open'],
+          investment: input.investment,
+          date: currentDate,
+        };
+        // TODO: use stock symbol here to use for tableau interface?
 
-          // append new Stock to array => this will then UPDATE the props in Stock List
-          setStocks(stocks => [...stocks, newStock]);
-
-        }
-      )
-
+        // append new Stock to array => this will then UPDATE the props in Stock List
+        setStocks((stocks) => [...stocks, newStock]);
+      });
   }
 
   function getStockOpen(stock) {
     let API_Call = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${stock}&outputsize=full&apikey=${API_KEY}`;
     //console.log(API_Call);
     fetch(API_Call)
-      .then(
-        function(response){
-          return response.json();
-        }
-      )
-      .then(
-        function(data){
-          //console.log("getStockOpen():",data)
-          let timeSeries = data["Time Series (Daily)"];
-          //console.log(timeSeries);
-          let latestDate = Object.keys(timeSeries).reduce(function(a, b){ return timeSeries[a] > timeSeries[b] ? b : a });
-          
-          let openPrice = timeSeries[latestDate];
-          console.log(openPrice);
-          let delta = 0
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        //console.log("getStockOpen():",data)
+        let timeSeries = data['Time Series (Daily)'];
+        //console.log(timeSeries);
+        let latestDate = Object.keys(timeSeries).reduce(function (a, b) {
+          return timeSeries[a] > timeSeries[b] ? b : a;
+        });
 
-          stocks.forEach(function (currStock) {
-            console.log(currStock);
-            console.log(stock);
-            if (currStock.symbol === stock) {
-              console.log("investment:", currStock.investment, "investment price:", currStock.price, "NEW price:", openPrice["1. open"] );
-              let net = parseFloat(currStock.investment) / parseFloat(currStock.price) * parseFloat(openPrice["1. open"]); 
-              delta = net - parseFloat(currStock.investment);
-              console.log(currStock.symbol, delta.toFixed(2));
-            }
-          });
+        let openPrice = timeSeries[latestDate];
+        console.log(openPrice);
+        let delta = 0;
 
-          let newOpenPrice = {
-            symbol: stock,
-            dailyOpenPrice: openPrice["1. open"],
-            netGain: delta.toFixed(2)
+        stocks.forEach(function (currStock) {
+          console.log(currStock);
+          console.log(stock);
+          if (currStock.symbol === stock) {
+            console.log(
+              'investment:',
+              currStock.investment,
+              'investment price:',
+              currStock.price,
+              'NEW price:',
+              openPrice['1. open']
+            );
+            let net =
+              (parseFloat(currStock.investment) / parseFloat(currStock.price)) *
+              parseFloat(openPrice['1. open']);
+            delta = net - parseFloat(currStock.investment);
+            console.log(currStock.symbol, delta.toFixed(2));
           }
+        });
 
-          // console.log(newOpenPrice);
-          setNetGains(netGains => [... netGains, newOpenPrice]);
-        }
-      )
+        let newOpenPrice = {
+          symbol: stock,
+          dailyOpenPrice: openPrice['1. open'],
+          netGain: delta.toFixed(2),
+        };
+
+        // console.log(newOpenPrice);
+        setNetGains((netGains) => [...netGains, newOpenPrice]);
+      });
   }
 
   // Callback function to pass to each individual stock in the list.
@@ -166,16 +166,16 @@ export default function Dashboard() {
     var d = new Date();
     // month is 0-indexed
     let m = d.getMonth() + 1;
-    let month = (m < 10) ? "0" + m.toString() : m.toString();
+    let month = m < 10 ? '0' + m.toString() : m.toString();
 
     // get the latest WEEKDAY
     // could be a little buggy on weekends bc stocks do not have a weekend value
     let date = d.getDate();
     if (d.getDay() === 6) date = date - 1;
     if (d.getDay() === 0) date = date - 2;
-    date = (date < 10) ? "0" + date.toString() : date.toString();
+    date = date < 10 ? '0' + date.toString() : date.toString();
 
-    return d.getFullYear().toString() + "-" + month + "-" + date;
+    return d.getFullYear().toString() + '-' + month + '-' + date;
   }
 
   function handleSaveStocks(stocks) {
@@ -185,44 +185,69 @@ export default function Dashboard() {
   return (
     <Container fluid>
       <Row>
-        <Col>  
-            { stocks.length > 0 ? (
-              <div id="stock-investor">
-                <StockSearch onFormSubmit={onSymbolSubmit} />
-                <Button onClick={() => saveStocksToDB(currentUser, stocks)}>Save</Button>
-                <StockList stocks={stocks} onStockRemove={onStockRemove} netGains={netGains}/>
-                <InvestmentSankey stocks={stocks} investment={totalInvestment} />
-              </div> ) : (
-              <div>
-                <StockSearch onFormSubmit={onSymbolSubmit} />
-              </div>
-              ) }
-        </Col>
-        <Col>
-          <Card>
-            <Card.Body>
-              <h2 className="text-center mb-4">Profile</h2>
-              {error && <Alert variant="danger">{error}</Alert>}
-              <strong>Email:</strong> {currentUser.email}
-              <Link to="/update-profile" className="btn btn-primary w-100 mt-3">
-                Update Profile
-              </Link>
-            </Card.Body>
-          </Card>
-          <div className="w-100 text-center mt-2">
-            <Button variant="link" onClick={handleLogout}>
-              Log Out
-            </Button>
+        <Tabs>
+          <div label="Profolio">
+            <Row>
+              <Col mdPush={4} md={8}>
+                {stocks.length > 0 ? (
+                  <div id="stock-investor">
+                    <StockSearch onFormSubmit={onSymbolSubmit} />
+                    <Button
+                      className="button-save"
+                      onClick={() => saveStocksToDB(currentUser, stocks)}
+                    >
+                      Save
+                    </Button>
+                    <StockList
+                      stocks={stocks}
+                      onStockRemove={onStockRemove}
+                      netGains={netGains}
+                    />
+                    <InvestmentSankey
+                      stocks={stocks}
+                      investment={totalInvestment}
+                    />
+                  </div>
+                ) : (
+                  <div>
+                    <StockSearch onFormSubmit={onSymbolSubmit} />
+                  </div>
+                )}
+              </Col>
+              <Col md={4}>
+                <Card>
+                  <Card.Body>
+                    <h2 className="text-center mb-4">Profile</h2>
+                    {error && <Alert variant="danger">{error}</Alert>}
+                    <strong>Email:</strong> {currentUser.email}
+                    <Link
+                      to="/update-profile"
+                      className="btn btn-primary w-100 mt-3"
+                    >
+                      Update Profile
+                    </Link>
+                  </Card.Body>
+                </Card>
+                <div className="w-100 text-center mt-2">
+                  <Button variant="link" onClick={handleLogout}>
+                    Log Out
+                  </Button>
+                </div>
+              </Col>
+            </Row>
           </div>
 
-         
+          <div label="News">
+            <Row>
+              <Tableau style="width: 500px; height: 500px;" id="dow"></Tableau>
+            </Row>
+          </div>
 
-        </Col>
-        
-      </Row>
-      <Row>
-        <Tableau style='width: 500px; height: 500px;' id="dow" ></Tableau>
+          <div label="Prediction Page">
+            Nothing to see here, this tab is <em>extinct</em>!
+          </div>
+        </Tabs>
       </Row>
     </Container>
-  )
+  );
 }
