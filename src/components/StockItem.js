defcb9f5a8c8206602b862d3f6c2e38169c698f2
Philip <philip.nguyen@sjsu.edu>
2021-05-05 15:49:36 -0700

    fixed investment var and right aligned numbers

--- src/components/StockItem.js
+++ src/components/StockItem.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
 import { useTranslation } from 'react-i18next';
+import './Dashboard.css';
 
 const StockItem = ({ stock, onStockRemove, dailyOpen, netGain }) => {
   const { t } = useTranslation();
@@ -8,11 +9,11 @@ const StockItem = ({ stock, onStockRemove, dailyOpen, netGain }) => {
   return (
     <tr>
       <td>{stock.symbol}</td>
-      <td>${parseFloat(stock.price).toFixed(2)}</td>
-      <td>${parseFloat(stock.investment).toFixed(2)}</td>
+      <td className="price">${parseFloat(stock.price).toFixed(2)}</td>
+      <td className="price">${parseFloat(stock.investment).toFixed(2)}</td>
       <td>{stock.date}</td>
-      <td>${dailyOpen ? parseFloat(dailyOpen).toFixed(2) : parseFloat(stock.price).toFixed(2)}</td>
-      <td className={parseFloat(netGain) >= 0 ? "text-success" : "text-danger"} >${netGain ? netGain : zero.toFixed(2)}</td>
+      <td className="price">${dailyOpen ? parseFloat(dailyOpen).toFixed(2) : parseFloat(stock.price).toFixed(2)}</td>
+      <td className={parseFloat(netGain) >= 0 ? "price text-success" : "price text-danger"} >${netGain ? netGain : zero.toFixed(2)}</td>
       <td><Button onClick={() => onStockRemove(stock)}>{t('remove_str')}</Button></td>
     </tr>
   )
